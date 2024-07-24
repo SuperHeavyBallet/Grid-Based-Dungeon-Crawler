ba60a80809d07ab2eba8154f547f1ea981c09f08
SuperHeavyBallet <agram@hotmail.co.uk>
2024-07-25 01:57:41 +0200

    Player Move System

--- GridDungeon/Assets/PlayerMovement.cs
+++ GridDungeon/Assets/PlayerMovement.cs
@@ -9,7 +9,13 @@ public class PlayerMovement : MonoBehaviour
     public PlayerInput playerInput;
     InputAction moveAction;
 
+    public int playerMovePoints = 3;
+
     public Vector2 playerPosition;
+
+    public bool isFreeUp, isFreeDown, isFreeRight, isFreeLeft;
+
+    public NeighbourSquareCollector neighbourSquareCollector;
 
 
     // Start is called before the first frame update
@@ -22,20 +28,120 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        if (moveValue != new Vector2(0, 0))
+        if (neighbourSquareCollector.squareIsUp)
         {
-            Move(moveValue);
+            isFreeUp = true;
         }
-        
+        else { isFreeUp = false; }
+
+        if (neighbourSquareCollector.squareIsDown)
+        {
+            isFreeDown = true;
+        }
+        else { isFreeDown = false; }
+
+        if (neighbourSquareCollector.squareIsLeft)
+        {
+            isFreeLeft = true;
+        }
+        else { isFreeLeft = false; }
+
+        if (neighbourSquareCollector.squareIsRight)
+        {
+            isFreeRight = true;
+        }
+        else { isFreeRight = false; }
+
+
+       
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        { }
+
+        else if (context.performed)
+        { Debug.Log("Action was performed");
+
+            if (playerMovePoints >0)
+            {
+                Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+                if (moveValue.x > 0)
+                {
+                    moveValue.x = 1;
+                }
+                if (moveValue.x < 0)
+                {
+                    moveValue.x = -1;
+                }
+                if (moveValue.y > 0)
+                {
+                    moveValue.y = 1;
+                }
+                if (moveValue.y < 0)
+                {
+                    moveValue.y = -1;
+                }
+
+                if (moveValue.x > 0 && moveValue.y > 0 ||
+                        moveValue.x < 0 && moveValue.y < 0 ||
+                        moveValue.x > 0 && moveValue.y < 0 ||
+                        moveValue.x < 0 && moveValue.y > 0)
+                {
+                    Debug.Log("A CONFLICT!: " + moveValue);
+                    moveValue = Vector2.zero;
+                }
+
+                Debug.Log("Move: " + moveValue);
+
+                if (moveValue.x > 0 && isFreeRight)
+                {
+                    this.transform.position = new Vector2(this.transform.position.x + moveValue.x, this.transform.position.y + moveValue.y);
+                    playerMovePoints -= 1;
+                }
+
+                if (moveValue.x < 0 && isFreeLeft)
+                {
+                    this.transform.position = new Vector2(this.transform.position.x + moveValue.x, this.transform.position.y + moveValue.y);
+                    playerMovePoints -= 1;
+                }
+                if (moveValue.y > 0 && isFreeUp)
+                {
+                    this.transform.position = new Vector2(this.transform.position.x + moveValue.x, this.transform.position.y + moveValue.y);
+                    playerMovePoints -= 1;
+                }
+                if (moveValue.y < 0 && isFreeDown)
+                {
+                    this.transform.position = new Vector2(this.transform.position.x + moveValue.x, this.transform.position.y + moveValue.y);
+                    playerMovePoints -= 1;
+                }
+
+                
+                if (playerMovePoints == 0)
+                {
+                    Invoke("ResetMovePoints", 2);
+                }
+            }
+            
+                
+            
+
+            
+
+        }
+        else if (context.canceled)
+        {  }
+
+       
+
+       
 
     }
 
-    public void Move(Vector2 moveValue)
+    void ResetMovePoints()
     {
-        Debug.Log("Move: " + moveValue);
-        this.transform.position = new Vector2(this.transform.position.x + moveValue.x, this.transform.position.y + moveValue.y);
-
-  
+        playerMovePoints = 3;
     }
 }
